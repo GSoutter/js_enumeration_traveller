1b097c70665087ce650cf6ffc30ad9c20c24894d
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-15 16:33:06 +0100

    adds calculateTotalDistanceTravelled

--- models/traveller.js
+++ models/traveller.js
@@ -22,7 +22,7 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {return total + journey.distance},0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
